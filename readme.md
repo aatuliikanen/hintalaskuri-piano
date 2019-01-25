7462c509ac258c1465fc8491369bf0a1def21fd7
Aatu Liikanen <aatu.liikanen@gmail.com>
2019-01-25 13:54:50 +0200

    Laskurin ensimmäinen versio

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Hintalaskuri muuttofirmalle
+
+Hintalaskuri on nettisivuilla oleva lomake, jonka täyttämällä kävijä saa tietää pianon kuljetuksen hinnan. 
+
+## Miten pianon hinta on laskettu?
+Hinta:
++ Perusmaksu: 100€
++ Flyygeli: 250€ (normaali piano +0€)
++ Kilometrit: 1€/km
++ Kerros alussa 2 tai enemmän: 50€
++ Kerros lopussa 2 tai enemmän: 50€
++ Hissi vähentää kerroksen hintalisäystä alussa ja lopussa: -25€
+
+### Kehitys
+Laskuri on kehitysvaiheessa. Tässä muutamia korjattavia ongelmia:
+- Sivu on päivitettävä, jos haluaa laskea hinnan uudelleen
+- Kun hinta tulee esille, sivun taustakuva heilahtaa.
+
---
+++ script.js
@@ -0,0 +1,96 @@
+
+// Price calculation
+
+// Perusmaksu 100€
+let pohja = 100
+
+// Pianotyyppi: normaali +0, flyygeli +250
+let pianoTyyppi = function () {
+	let tyyppi = document.getElementsByName("piano");
+	let hinta = 0;
+	if(tyyppi[1].checked) {
+		hinta = 250;
+	} else {
+		hinta = 0;
+	}
+	return hinta;
+}
+
+// Matkalasku on matka x 1€
+let matkalasku = function() {
+	a = document.getElementById("matka").value
+	return a;
+}
+
+
+// Ykköskerros +0€, muut +50€
+
+let kerrosOne = function() {
+	let k1 = document.getElementById("kerros1").value;
+	if(k1 == 1) {
+		return 0;
+	} else {
+		return 50;
+	}
+}
+
+let kerrosTwo = function() {
+	let k2 = document.getElementById("kerros2").value;
+	if(k2 == 1) {
+		return 0;
+	} else {
+		return 50;
+	}
+}
+
+// Hissi: jos yli 2 kerrosta, niin hinta vaikutus miinus 40€
+
+let hissi1 = function() {
+	let h1 = document.getElementById("hissi1").value;
+	if(h1 == "Kyllä" && kerrosOne() > 25) {
+		return -25;
+	} else {
+		return 0;
+	}
+}
+
+let hissi2 = function() {
+	let h2 = document.getElementById("hissi2").value;
+	if(h2 == "Kyllä" && kerrosTwo() > 25) {
+		return (-25);
+	} else {
+		return 0;
+	}
+}
+
+// Kokonaishinta
+
+let total = function() {
+	total = Number(pohja) + Number(pianoTyyppi()) + Number(matkalasku()) + Number(kerrosOne())
+	+ Number(kerrosTwo()) + Number(hissi1()) + Number(hissi2());
+	return total;
+}
+
+
+
+
+// What happens after clicking a button
+
+let newAlert = function(message) {
+	let newElement = document.createElement('div');
+	newElement.className = 'newElement';
+	newElement.appendChild(document.createTextNode(message));
+	let laskuri = document.querySelector('.laskuri');
+	let vastaus = document.querySelector('#vastaus');
+	laskuri.insertBefore(newElement, vastaus);
+}
+
+
+
+let button = document.getElementById("nappi");
+
+button.addEventListener("click", function() {
+	newAlert('Kuljetuksen hinta on ' + total() + ' euroa.' );
+})
+
+
