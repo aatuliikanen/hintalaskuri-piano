ad5634b24502e75e9f8f79f1f9ec27c3baf0cd8f
Aatu Liikanen <aatu.liikanen@gmail.com>
2019-02-22 12:47:37 +0200

    päivitys

--- script.js
+++ script.js
@@ -23,50 +23,33 @@ function hintaMatka() {
 }
 
 // Kerroksen hintavaikutus
-function hintaKerros1() {
-	let kerros1 = document.getElementById("kerros1").value;
-	if(kerros1 == 1) {
-		return 0;
-	} else if(kerros1 == 2) {
-		return 30;
-	} else {
-		return 50;
-	}
-}
-
-function hintaKerros2() {
-	let kerros2 = document.getElementById("kerros2").value;
-	if(kerros2 == 1) {
-		return 0;
-	} else if(kerros2 == 2) {
-		return 30;
-	} else {
-		return 50;
-	}
-}
 
 // Hissin hintavaikutus
-function hintaHissi1() {
+function hintaKerros1() {
 	let hissi1 = document.getElementById("hissi1").value;
 	let kerros1 = document.getElementById("kerros1").value;
 
 	if(hissi1 == "Kyllä" && kerros1 > 1) {
-		return 50;
-	} else if(hissi1 == "Ei" && kerros1 > 1) {
-		return 100;
+		return 30;
+	} else if(hissi1 == "Ei" && kerros1 == 2) {
+		return 70;
+	} else if(hissi1 == "Ei" && kerros1 >= 3) {
+		return 100
 	} else {
 		return 0
 	}
 }
 
-function hintaHissi2() {
+function hintaKerros2() {
 	let hissi2 = document.getElementById("hissi2").value;
 	let kerros2 = document.getElementById("kerros2").value;
 
 	if(hissi2 == "Kyllä" && kerros2 > 1) {
-		return 50;
-	} else if(hissi2 == "Ei" && kerros2 > 1) {
-		return 100;
+		return 30;
+	} else if(hissi2 == "Ei" && kerros2 == 2) {
+		return 70;
+	} else if(hissi2 == "Ei" && kerros2 >= 3) {
+		return 100
 	} else {
 		return 0
 	}
@@ -76,7 +59,7 @@ function hintaHissi2() {
 
 function getPrice() {
 	let total = Number(perusmaksu) + Number(hintaPianotyyppi()) + Number(hintaMatka()) + Number(hintaKerros1())
-	+ Number(hintaKerros2()) + Number(hintaHissi1()) + Number(hintaHissi2());
+	+ Number(hintaKerros2());
 
 	return total;
 }
@@ -85,7 +68,7 @@ function createDiv() {
 	let newDiv = document.getElementById('new-div');
 
 	newDiv.innerHTML= `
-					<p class="hintateksti">Kuljetuksen hinta on ${getPrice()} euroa. Hinta sisältää arvonlisäveron</p>
+					<p class="hintateksti">Kuljetuksen hinta on ${getPrice()} euroa (sis. alv).</p>
 					<div class="new-form">
 						<h5>Haluatko tilata kuljetuksen pianolle?</h5>
 						<p>Jätä puhelinnumero, niin otamme yhteyttä.</p>
