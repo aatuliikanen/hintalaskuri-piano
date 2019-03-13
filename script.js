e43afc2eec1d8f9a48db79cb57d0657823678f6b
Aatu Liikanen <aatu.liikanen@gmail.com>
2019-03-13 09:25:26 +0200

    päivitys

--- script.js
+++ script.js
@@ -1,88 +1,161 @@
 // HINNAN LASKEMINEN
 
 // Perusmaksu
-let perusmaksu = 100
+let perusmaksu = 150;
 
 // Pianon tyyppi
 function hintaPianotyyppi() {
-	let tyyppi = document.querySelector('input[name="piano"]:checked').value;
+	let tyyppi = document.querySelector('input[name="pianotyyppi"]:checked').value;
 
-	if(tyyppi == 'piano') {
+	if(tyyppi === 'piano') {
 		return 0;
-	} else {
-		// Paljonko lisähintaa flyygelistä
+	} else if(tyyppi == 'flyygeli'){
 		return 250
+	} else {
+		return 0;
 	}
 }
 
-// Matkalasku
-function hintaMatka() {
-	let matka = document.getElementById("matka").value
-	// Paljonko hintaa per kilometri
-	return matka * 1;
+// Etäisyys
+
+function hintaDistance1() {
+	let distance1 = document.getElementById('distance-1').value;
+
+	if(distance1 === 'alle-30') {
+		return 0;
+	} else if(distance1 === 'alle-60') {
+		return 50;
+	} else if(distance1 === 'alle-90') {
+		return 100;
+	}
+}
+
+function hintaDistance2() {
+	let distance2 = document.getElementById('distance-2').value;
+
+	if(distance2 === 'alle-30') {
+		return 0;
+	} else if(distance2 === 'alle-60') {
+		return 50;
+	} else if(distance2 === 'alle-90') {
+		return 100;
+	}
 }
 
 // Kerroksen hintavaikutus
 
-// Hissin hintavaikutus
 function hintaKerros1() {
-	let hissi1 = document.getElementById("hissi1").value;
-	let kerros1 = document.getElementById("kerros1").value;
+	let kerros1 = document.getElementById('portaat-1').value;
 
-	if(hissi1 == "Kyllä" && kerros1 > 1) {
-		return 30;
-	} else if(hissi1 == "Ei" && kerros1 == 2) {
+	if(kerros1 === 'ei') {
+		return 0;
+	} else if(kerros1 === '1-2') {
+		return 50;
+	} else if(kerros1 === '3-4') {
 		return 70;
-	} else if(hissi1 == "Ei" && kerros1 >= 3) {
-		return 100
-	} else {
-		return 0
+	} else if(kerros1 === 'yli-5') {
+		return 100;
 	}
 }
 
 function hintaKerros2() {
-	let hissi2 = document.getElementById("hissi2").value;
-	let kerros2 = document.getElementById("kerros2").value;
+	let kerros2 = document.getElementById('portaat-2').value;
 
-	if(hissi2 == "Kyllä" && kerros2 > 1) {
-		return 30;
-	} else if(hissi2 == "Ei" && kerros2 == 2) {
+	if(kerros2 === 'ei') {
+		return 0;
+	} else if(kerros2 === '1-2') {
+		return 50;
+	} else if(kerros2 === '3-4') {
 		return 70;
-	} else if(hissi2 == "Ei" && kerros2 >= 3) {
-		return 100
-	} else {
-		return 0
+	} else if(kerros2 === 'yli-5') {
+		return 100;
 	}
 }
 
-// VASTAUSTEKSTI
+// KOKONAISHINTA
 
-function getPrice() {
-	let total = Number(perusmaksu) + Number(hintaPianotyyppi()) + Number(hintaMatka()) + Number(hintaKerros1())
-	+ Number(hintaKerros2());
+function laskeHinta() {
+	let total = Number(perusmaksu) + Number(hintaPianotyyppi()) + Number(hintaDistance1()) + Number(hintaDistance2())
+	+ Number(hintaKerros1()) + Number(hintaKerros2());
 
-	return total;
+	return Math.round(total);
 }
 
 function createDiv() {
 	let newDiv = document.getElementById('new-div');
 
 	newDiv.innerHTML= `
-					<p class="hintateksti">Kuljetuksen hinta on ${getPrice()} euroa (sis. alv).</p>
-					<div class="new-form">
-						<h5>Haluatko tilata kuljetuksen pianolle?</h5>
-						<p>Jätä puhelinnumero, niin otamme yhteyttä.</p>
-						<form id="puhelin">
-							<p>
-								<label for="phone">Puhelin</label>
-								<input type="tel" id="phone">
-							</p>
-							<p>
-								<button type="button" id="tilaa" onclick="send()">Lähetä</button>
-							</p>
-						</form>
-					</div>
-					<div class="thankyou"></div>
+		<p>Kuljetuksen hinta on ${laskeHinta()} euroa. Hinta sisältää arvonlisäveron 24%.</p>
+		<p>Työn sisältö</p>
+		<ul>
+		  <li>Pianon suojaus</li>
+		  <li>Hellävarainen kuljetus</li>
+		  <li>Pianon asettelu haluttuun paikkaan</li>
+		</ul>
+		<h4>Tilaa pianon muutto</h4>
+		<p>Valitse muuttopäivä ja jätä yhteystietosi. Saat tilauksesta
+		vahvistuksen sähköpostiisi.</p>
+		<p>
+			<label>Muuttopäivä
+			<input type="text" name="muuttopäivä">
+			</label>
+		</p>
+		<p>
+			<label>Kommentit
+			<textarea name="kommentti" rows="3"></textarea>
+			</label>
+		</p>
+		<h5>Yhteystiedot</h5>
+		<p>
+			<label>Nimi
+			<input type="text" name="nimi">
+			</label>
+		</p>
+		<p>
+			<label>Osoite (lähtöpaikka)
+			<input type="text" name="osoite">
+			</label>
+		</p>
+		<p>
+			<label>Postinumero
+			<input type="number" name="postinumero">
+			</label>
+		</p>
+		<p>
+			<label>Paikkakunta
+			<input type="text" name="paikkakunta">
+			</label>
+		</p>
+		<p>
+			<label>Puhelinnumero
+				<input type="number" name="Puhelinnumero">
+			</label>
+		</p>
+		<p>
+			<label>Sähköposti
+			<input type="email" name="sahkoposti">
+			</label>
+		</p>
+		<h5>Muuttokohteen osoite</h5>
+		<p>
+			<label>Osoite
+			<input type="text" name="osoite">
+			</label>
+		</p>
+		<p>
+			<label>Postinumero
+			<input type="number" name="postinumero">
+			</label>
+		</p>
+		<p>
+			<label>Paikkakunta
+			<input type="text" name="paikkakunta">
+			</label>
+		</p>
+		<p>
+			<button type="submit">Tilaa</button>
+		</p>
+		<p>Valitettavasti emme muuta oikeasti pianoasi. Valitan.</p>
 		`
 		
 		newDiv.style.padding = "20px";
